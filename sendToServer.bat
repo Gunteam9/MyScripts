56e85f324a87a14f08549ae808ecac1b1d565c67
Gunteam9 <58592342+Gunteam9@users.noreply.github.com>
2022-12-08 13:49:11 +0100

    Update post build event and scp script

--- sendToServer.bat
+++ sendToServer.bat
@@ -1,1 +1,6 @@
-scp -r %TMP%/gta/. debian@51.91.98.193:/FxServer/server-data/resources/[my]
+if %ComputerName%==DESKTOP-VNP7NL1 goto copyDebian
+if %ComputerName%== goto copyLadislus
+:copyDebian
+scp -r %TMP%/[my] debian@51.91.98.193:/FxServer/server-data/resources
+:copyLadilsus
+scp -r %TMP%/[my] ladislus@51.91.98.193:/FxServer/server-data/resources
